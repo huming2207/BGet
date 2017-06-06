82220872b3642816cf7cdb2a6f34bb011217ffb0
Jackson Ming Hu <huming2207@gmail.com>
2017-06-06 19:38:37 +1000

    Implement unit test sample

---
+++ BoggerCore/BoggerExceptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoggerCore
+{
+    public class VideoInfoNotFoundException : Exception
+    {
+        public VideoInfoNotFoundException() { }
+
+        public VideoInfoNotFoundException(string message) : base(message) { }
+
+        public VideoInfoNotFoundException(string message, Exception inner) : base(message, inner) { }
+
+        public override string Message { get { return "Video not found!"; } }
+    }
+
+    public class VideoUrlNotFoundException : Exception
+    {
+        public VideoUrlNotFoundException() { }
+
+        public VideoUrlNotFoundException(string message) : base(message) { }
+
+        public VideoUrlNotFoundException(string message, Exception inner) : base(message, inner) { }
+
+        public override string Message { get { return "Cannot fetch video URL!"; } }
+    }
+}
--- BoggerTest/BoggerVideoInfoTest.cs
+++ BoggerTest/BoggerVideoInfoTest.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using BoggerCore;
 
 namespace BoggerTest
 {
     public class BoggerVideoInfoTest
     {
+        private VideoInfoCrawler videoInfoCrawler = null;
+
+        public BoggerVideoInfoTest()
+        {
+            videoInfoCrawler = new VideoInfoCrawler();
+        }
+
         [Fact]
         public void ValidVideoInfoTest()
         {
-            
+            // Video URL: http://www.bilibili.com/video/av349183/
+            // "Onepublic - Feel Again MV", Content ID 540559
+            var videoInfoForAv349183 = videoInfoCrawler.GetVideoInfo("av349183").Result;
+            Assert.NotNull(videoInfoForAv349183);
+            Assert.True(videoInfoForAv349183.ContentId.Equals("540559"));
+            Console.Read();
         }
     }
 }
