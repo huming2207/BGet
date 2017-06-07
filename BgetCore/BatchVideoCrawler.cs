042717972fce560b050181eb25ee7496b36c2f13
Jackson Ming Hu <huming2207@gmail.com>
2017-06-07 17:27:51 +1000

    Initially implement some User related stuff

--- BgetCore/BatchVideoCrawler.cs
+++ BgetCore/BatchVideoCrawler.cs
@@ -1,10 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace BgetCore
 {
-    class BatchVideoCrawler
+    public class BatchVideoCrawler
     {
+        public async Task<List<VideoInfo>> GetAllVideoInfoByKeyword(string keyword)
+        {
+
+        }
+
+        public async Task<List<int>> GetAllUserIdFromKeyword(string keyword)
+        {
+
+        }
+
+        private async Task<SearchResult> _GetAllVideoByKeyword(string keyword)
+        {
+            /* 
+             * Search API format: http://search.bilibili.com/ajax_api/video?keyword=keywordsB&page=10&order=totalrank&_=1496810721252 
+                    
+                    Parameter: - keyword: keyword (encoded string)
+                               - page: page number (unsigned int)
+                               - order: keep "totalrank" since it works... (string)
+                               - _: unix timestamp in ms (unsigned int64?)
+             
+             */
+        }
+
+        private async Task<SearchResult> _GetAllUserByKeyword(string keyword)
+        {
+            /* 
+             * Search API format: http://search.bilibili.com/ajax_api/upuser?keyword=keywordsB&page=10&order=totalrank&_=1496810721252 
+
+                    Parameter: - keyword: keyword (encoded string)
+                               - page: page number (unsigned int)
+                               - order: keep "totalrank" since it works... (string)
+                               - _: unix timestamp in ms (unsigned int64?)
+
+             */
+        }
+
+
+
+
+
+
+
     }
 }
