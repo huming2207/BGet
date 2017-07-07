4ff6de44a827dc67e5e27f063eb35902ff8018d7
Jackson Ming Hu <huming2207@gmail.com>
2017-07-07 16:06:35 +1000

    GUI: Drop UWP, WPF init

---
+++ BgetUwp/Helpers/ResourceExtensions.cs
@@ -0,0 +1,16 @@
+using System.Runtime.InteropServices;
+
+using Windows.ApplicationModel.Resources;
+
+namespace BgetUwp.Helpers
+{
+    internal static class ResourceExtensions
+    {
+        private static ResourceLoader _resLoader = new ResourceLoader();
+
+        public static string GetLocalized(this string resourceKey)
+        {
+            return _resLoader.GetString(resourceKey);
+        }
+    }
+}
