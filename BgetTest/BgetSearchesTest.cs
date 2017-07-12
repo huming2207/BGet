1c5758b8a143dde8d1b83efd4c14806d92b9a00b
Jackson Ming Hu <huming2207@gmail.com>
2017-07-12 22:02:01 +1000

    Search Test: Let the damn Xunit test discover my UserSearch Test...

---
+++ BgetTest/BgetSearchesTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using BgetCore.Search;
+using Xunit;
+
+namespace BgetTest
+{
+    public class BgetSearchesTest
+    {
+        private Search _search;
+        
+        public BgetSearchesTest()
+        {
+            _search = new Search();
+        }
+
+        [Fact]
+        public async Task BgetUserSearchTest()
+        {
+            var userSearchResult = await _search.GetAllUserIdFromKeyword("大米评测");
+            
+            Assert.NotNull(userSearchResult);
+            Assert.True(userSearchResult.Count > 0);
+        }
+
+        [Fact]
+        public async Task BgetVideoSearchTest()
+        {
+            var videoSearchResult = await _search.GetAllVideoInfoByKeyword("局座");
+            
+            Assert.NotNull(videoSearchResult);
+            Assert.True(videoSearchResult.Count > 0);
+        }
+        
+    }
+}
