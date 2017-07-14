1d8034e5ef65da66152102ecf51d784fc1590673
Jackson Ming Hu <huming2207@gmail.com>
2017-07-14 11:59:33 +1000

    UserVideo: Bugfixes on 404

--- BgetCore/User/UserVideoGrabber.cs
+++ BgetCore/User/UserVideoGrabber.cs
@@ -12,12 +12,14 @@ namespace BgetCore.User
         public async Task<List<UserVideo>> GetAllVideoFromUser(string userId, string keyword = "")
         {
             // Set the initial page to 1
-            int currentPage = 1;
+            var currentPage = 1;
+            
             var userVideoList = new List<UserVideo>();
             var userVideo = await GetVideoFromUser(userId, 100, currentPage, keyword);
+            var totalPages = userVideo.UploadedVideo.Pages;
 
             // "Iterate" the user video page
-            while(userVideo.Status)
+            while (userVideo.Status && currentPage <= totalPages)
             {
                 userVideoList.Add(userVideo);
 
@@ -33,20 +35,20 @@ namespace BgetCore.User
             // Declare HTTP client
             var httpClient = new HttpClient()
             {
-                BaseAddress = new Uri("http://space.bilibili.com/ajax"),
+                BaseAddress = new Uri("http://space.bilibili.com/"),
             };
 
             // Set referrer in case HTTP 400 when accessing User APIs
             httpClient.DefaultRequestHeaders.Referrer = new Uri(string.Format("http://space.bilibili.com/{0}", userId));
 
+            // Force using Internet Explorer 10's user agent to get the flash version instead of pure HTML5 version
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)");
+
             // Fire the hole!
             var responseJson = await httpClient.GetStringAsync(
-                string.Format("/member/getSubmitVideos?mid={0}&pagesize={1}&tid=0&page={2}&keyword={3}&order=senddate&_={4}", 
-                                userId, 
-                                pageSize.ToString(), 
-                                page.ToString(), 
-                                keyword, 
-                                DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString()));
+                $"ajax/member/getSubmitVideos?mid={userId}&pagesize={pageSize.ToString()}" +
+                $"&tid=0&page={page.ToString()}&keyword={keyword}&order=senddate" +
+                $"&_={DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString()}");
 
             // Convert to object
             var jsonObject = JsonConvert.DeserializeObject<UserVideo>(responseJson);
--- BgetTest/BgetUserVideoTest.cs
+++ BgetTest/BgetUserVideoTest.cs
@@ -1,7 +1,43 @@
+﻿using System.Threading.Tasks;
+using BgetCore.User;
+using Xunit;
+
 namespace BgetTest
 {
     public class BgetUserVideoTest
     {
-        
+        private UserVideoGrabber _userVideoGrabber;
+
+        public BgetUserVideoTest()
+        {
+            _userVideoGrabber = new UserVideoGrabber();
+        }
+
+        [Fact]
+        public async Task BgetValidUserVideoListTest()
+        {
+            // 大米评测, a phone review channel.
+            // URL: http://space.bilibili.com/8372353
+            var resultList = await _userVideoGrabber.GetAllVideoFromUser("8372353");
+
+            Assert.NotNull(resultList);
+            Assert.True(resultList.Count > 0);
+
+            // See if each result is correct
+            foreach (var result in resultList)
+            {
+                Assert.True(result.Status);
+            }
+        }
+
+        [Fact]
+        public async Task BgetInvalidUserVideoListTest()
+        {
+            // ...just a fake ID
+            var result = await _userVideoGrabber.GetAllVideoFromUser("99999999999999");
+
+            Assert.NotNull(result);
+            Assert.True(!result[0].Status);
+        }
     }
 }
