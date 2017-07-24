41b1ead264a7475fa2ad633a8aa8dce0815ff311
Jackson Ming Hu <huming2207@gmail.com>
2017-07-24 22:30:08 +1000

    Model: Add BGet model, not yet tested.
    ...BTW hope you can recovery from the flu soon lol

---
+++ BgetWpf/Model/BgetModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BgetCore.Video;
+
+namespace BgetWpf.Model
+{
+    public class BgetModel
+    {
+        /// <summary>
+        /// Generate video link
+        /// 
+        /// Note:
+        /// 1. UserAgent (MSIE10) and Referrer must be set to the downloader, otherwise server will return 500
+        /// 2. Exception may be raised if something goes wrong (e.g. no video found)
+        /// </summary>
+        /// <param name="contentUrl"></param>
+        /// <returns></returns>
+        private async Task<List<string>> GenerateVideoLink(string contentUrl)
+        {
+            // BGetCore library related stuff
+            var videoInfoCrawler = new VideoInfoCrawler();
+            var videoInfo = await videoInfoCrawler.GetVideoInfo(contentUrl);
+
+            var videoUrlGrabber = new VideoUrlCrawler();
+            var videoUrl = await videoUrlGrabber.GetUrlBySingleContentId(videoInfo);
+
+            var urlList = new List<string>();
+
+            // Just a practice of LINQ...
+            if (Properties.Settings.Default.PreferFlv)
+            {
+                videoUrl.Durl.ForEach(url => urlList.Add(url.Url));
+            }
+            else
+            {
+                urlList.AddRange(
+                    videoUrl.Durl.Where(url => url.BackupUrl != null && url.BackupUrl.Url.Count != 0)
+                        .SelectMany(url => url.BackupUrl.Url));
+            }
+
+            return urlList;
+        }
+    }
+}
--- BgetWpf/Model/ConstValue.cs
+++ BgetWpf/Model/ConstValue.cs
@@ -8,7 +8,5 @@ namespace BgetWpf.Model
     {
         public const string DefaultUserAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)";
 
-        public static string SettingPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) +
-                                           "/setting.json";
     }
 }
