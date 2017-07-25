48dbbf56241e305d9675a25eabf5f31eaa635cb3
Jackson Ming Hu <huming2207@gmail.com>
2017-07-25 17:55:02 +1000

    GUI: Update model

--- BgetWpf/Model/BgetModel.cs
+++ BgetWpf/Model/BgetModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BgetCore.User;
+using BgetCore.User.UserResult;
 using BgetCore.Video;
 
 namespace BgetWpf.Model
@@ -18,7 +21,7 @@ namespace BgetWpf.Model
         /// </summary>
         /// <param name="contentUrl"></param>
         /// <returns></returns>
-        private async Task<List<string>> GenerateVideoLink(string contentUrl)
+        private async Task<List<string>> GenerateGeneralVideoLink(string contentUrl)
         {
             // BGetCore library related stuff
             var videoInfoCrawler = new VideoInfoCrawler();
@@ -39,6 +42,49 @@ namespace BgetWpf.Model
                 urlList.AddRange(
                     videoUrl.Durl.Where(url => url.BackupUrl != null && url.BackupUrl.Url.Count != 0)
                         .SelectMany(url => url.BackupUrl.Url));
+            }
+
+            return urlList;
+        }
+
+        /// <summary>
+        /// Generate video links for all videos from a certain user.
+        /// This method may costs quite a long time. As a result, a IProgess is implemented to retrieve the status
+        /// 
+        /// Note:
+        /// 1. UserAgent (MSIE10) and Referrer must be set to the downloader, otherwise server will return 500
+        /// 2. Exception may be raised if something goes wrong (e.g. no video found)
+        /// 3. Status reporting example is here: https://stackoverflow.com/questions/19980112/how-to-do-progress-reporting-using-async-await/19980151#19980151
+        ///     The format is 
+        /// </summary>
+        /// <param name="contentUrl"></param>
+        /// <param name="progressStatus"></param>
+        /// <returns></returns>
+        private async Task<List<string>> GenerateUserVideoLink(string contentUrl, IProgress<double[]> progressStatus)
+        {
+            // BGet library related stuff
+            var userVideoGrabber = new UserVideoGrabber();
+            var userVideoResult = await userVideoGrabber.GetAllVideoFromUser(contentUrl);
+
+            // Create a list to get all video links
+            var urlList = new List<string>();
+
+            for (var currentVideoPage = 0; currentVideoPage < userVideoResult.Count; currentVideoPage++)
+            {
+                for (var currentVideo = 0; currentVideo < userVideoResult[currentVideoPage].UploadedVideo.VideoList.Count; currentVideo++)
+                {
+
+                    var video = userVideoResult[currentVideoPage].UploadedVideo.VideoList[currentVideo];
+                    urlList.AddRange(await GenerateGeneralVideoLink(video.ContentId));
+                    
+                    // Report the status 
+                    progressStatus.Report(
+                        new[]
+                        {
+                            (currentVideoPage / (double)userVideoResult.Count),
+                            (currentVideo / (double)userVideoResult[currentVideoPage].UploadedVideo.VideoList.Count)
+                        });
+                }
             }
 
             return urlList;
